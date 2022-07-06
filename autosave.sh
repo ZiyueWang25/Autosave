6d165fd0f5aa670d23414195505fa4deafbdc300
Vincent <vincent.wang.baruchmfe@gmail.com>
2022-07-06 07:58:54 -0700

    add time and blank information

--- autosave.sh
+++ autosave.sh
@@ -5,13 +5,21 @@ SLEEP_TIME=$2
 while true
 do
  ACTIVE_APP=$(xdotool getwindowfocus getwindowname);
- if [[ $ACTIVE_APP == *$AUTOSAVE_APP* ]];
- then 
-  echo $ACTIVE_APP
-  $(xdotool key ctrl+s);
-  echo "Autosave successfully"
+ SCREEN_INFO=$(xset q);
+ TIME=$(date +"%d-%m-%y %T");
+ echo -n "${TIME} "
+ if [[ $SCREEN_INFO == *"Monitor is On"* ]];
+ then
+  if [[ $ACTIVE_APP == *$AUTOSAVE_APP* ]];
+  then 
+   echo $ACTIVE_APP
+   $(xdotool key ctrl+s);
+   echo "Autosave successfully"
+  else
+   echo "Not ${AUTOSAVE_APP}, is ${ACTIVE_APP}"
+  fi;
  else
-  echo "Not ${AUTOSAVE_APP}"
+  echo "Screen is blank"
  fi;
  sleep $SLEEP_TIME;
 done
